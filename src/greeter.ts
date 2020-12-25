76d3b0b2040451a0b39d79029cb1d21ef9749d0a
youtiao66 <978568519@qq.com>
2020-12-25 14:28:34 +0800

    feat: TypeScript Tooling in 5 minutes

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,3 +1,15 @@
+class Student {
+  fullName: string
+
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
+  }
+}
+
 interface Person {
   firstName: string,
   lastName: string
@@ -7,6 +19,6 @@ function greeter(person: Person) {
   return 'Hello, ' + person.firstName + ' ' + person.lastName
 }
 
-let user = { firstName: 'Jane', lastName: 'User' }
+let user = new Student('Jane', 'M.', 'User')
 
 console.log(greeter(user))
