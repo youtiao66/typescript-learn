1d246723bcf9c6765ea9a66c4a1822979fb311af
youtiao66 <978568519@qq.com>
2021-01-20 11:51:18 +0800

    feat: class expression

--- src/interface-learn.ts
+++ src/interface-learn.ts
@@ -6,27 +6,12 @@ interface ClockConstructor {
   new (hour: number, minute: number): ClockInterface
 }
 
-function createClock(
-  ctor: ClockConstructor,
-  hour: number,
-  minute: number
-): ClockInterface {
-  return new ctor(hour, minute)
-}
-
-class DigitalClock implements ClockInterface {
-  constructor(h: number, m: number) {}
+const Clock: ClockConstructor = class Clock implements ClockInterface {
+  constructor(h: number, minute: number) {}
   tick() {
-    console.log('deep deep')
+    console.log('beep beep')
   }
 }
 
-class AnalogClock implements ClockInterface {
-  constructor(h: number, m: number) {}
-  tick() {
-    console.log('tick tock')
-  }
-}
-
-let digital = createClock(DigitalClock, 12, 17)
-let analog = createClock(AnalogClock, 7, 32)
+let clock = new Clock(7, 22)
+clock.tick()
