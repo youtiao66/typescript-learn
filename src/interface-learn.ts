1c01e90bb48db55203268f40b7370b982b6a6996
youtiao66 <978568519@qq.com>
2021-01-20 13:34:42 +0800

    feat: extend multiple interfaces

--- src/interface-learn.ts
+++ src/interface-learn.ts
@@ -2,10 +2,15 @@ interface Shape {
   color: string
 }
 
-interface Square extends Shape {
+interface PenStroke {
+  penWidth: number
+}
+
+interface Square extends Shape, PenStroke {
   sideLength: number
 }
 
 let square = {} as Square
 square.color = 'blue'
 square.sideLength = 10
+square.penWidth = 5.0
