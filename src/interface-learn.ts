d0bbb248288702f64babcd3ff84cf003d382d6f3
youtiao66 <978568519@qq.com>
2021-01-20 13:45:58 +0800

    feat: Hybrid Types

--- src/interface-learn.ts
+++ src/interface-learn.ts
@@ -1,16 +1,17 @@
-interface Shape {
-  color: string
+interface Counter {
+  (start: number): string
+  interval: number,
+  reset(): void
 }
 
-interface PenStroke {
-  penWidth: number
+function getCounter(): Counter {
+  let counter = function(start: number) {} as Counter
+  counter.interval = 123
+  counter.reset = function() {}
+  return counter
 }
 
-interface Square extends Shape, PenStroke {
-  sideLength: number
-}
-
-let square = {} as Square
-square.color = 'blue'
-square.sideLength = 10
-square.penWidth = 5.0
+let c = getCounter()
+c(10)
+c.reset()
+c.interval = 5.0
