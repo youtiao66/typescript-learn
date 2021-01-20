fb14bdef22436642254a4fe325315f592d7925fd
youtiao66 <978568519@qq.com>
2021-01-20 10:57:04 +0800

    feat: createClock

--- src/interface-learn.ts
+++ src/interface-learn.ts
@@ -1,14 +1,32 @@
 interface ClockInterface {
-  currentTime: Date
-  setTime(d: Date): void
+  tick(): void
 }
 
-class Clock implements ClockInterface {
-  currentTime: Date = new Date()
-  setTime(d: Date) {
-    this.currentTime = d
-  }
-  constructor(h: number, m: number) {
+interface ClockConstructor {
+  new (hour: number, minute: number): ClockInterface
+}
 
+function createClock(
+  ctor: ClockConstructor,
+  hour: number,
+  minute: number
+): ClockInterface {
+  return new ctor(hour, minute)
+}
+
+class DigitalClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('deep deep')
   }
 }
+
+class AnalogClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('tick tock')
+  }
+}
+
+let digital = createClock(DigitalClock, 12, 17)
+let analog = createClock(AnalogClock, 7, 32)
